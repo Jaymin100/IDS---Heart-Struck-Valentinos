c77feb1119dda1e4ed21ae4ea555b70469de1fb0
Saad Arshad Pervez Mughal <saadmughal6924@gmail.com>
2025-02-15 21:43:41 -0600

    updated buttons

--- 360-viewer/src/components/Sidebar.jsx
+++ 360-viewer/src/components/Sidebar.jsx
@@ -1,26 +1,24 @@
-// Sidebar.js (Corrected)
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Sidebar({ currentMode, onSelectMode, onAddHotspot }) {
   return (
     <div className="Sidebar d-flex flex-column gap-2 p-3">
-  <button
-    className={`btn btn-dark ${currentMode === "select" ? "active" : ""}`}
-    onClick={onSelectMode}
-    disabled={currentMode === "select"}
-  >
-    Select Mode
-  </button>
-  <button
-    className={`btn btn-dark ${currentMode === "addHotspot" ? "active" : ""}`}
-    onClick={onAddHotspot}
-    disabled={currentMode === "addHotspot"}
-  >
-    Add Hotspot Mode
-  </button>
-</div>
-
+      <button
+        className={`btn btn-dark ${currentMode === "select" ? "active" : ""}`}
+        onClick={onSelectMode}
+        disabled={currentMode === "select"}
+      >
+        Select Mode
+      </button>
+      <button
+        className={`btn btn-dark ${currentMode === "addHotspot" ? "active" : ""}`}
+        onClick={onAddHotspot}
+        disabled={currentMode === "addHotspot"}
+      >
+        Add Hotspot Mode
+      </button>
+    </div>
   );
 }
 
